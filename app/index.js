f434c22c8841e3285bdf155399d770baa99607c0
Orgil <orgil.u@gmail.com>
2017-05-12 10:31:39 +0800

    root removed

--- app/index.js
+++ app/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import Root from './Root';
 import TestComp from 'test-lib';
 
 const store = createStore(function() {});
